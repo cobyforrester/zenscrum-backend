7f642e649621c9ed41745c0d7c79a11d7758ad08
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-07-01 18:10:31 -0700

    added to database

--- projects/models.py
+++ projects/models.py
@@ -5,3 +5,6 @@ class Project(models.Model):
     title = models.TextField(default='AddTitle', blank=False, null=False)
     begin_date = models.DateField(default=date.today, blank=False, null=False)
     description = models.TextField(default='AddGoal', blank=False, null=False)
+class UserProject(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    username_project = models.TextField(default='username', blank=False, null=False)
--- projects/views.py
+++ projects/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 from .models import Project
 def projects_home_view(request, *args, **kwargs):
     return render(request, 'projects/projects.html', context={}, status=200)
+
+def project_details(request, project_number, *args, **kwargs):
+    '''
+    REST API VIEW
+    Consume by JavaScript/React
+    return Json
+    '''
+    data = {
+        #'image': obj.image.url
+}
+    try:
+        obj = Project.objects.get(id=project_number)
+        data['date'] = obj.date
+        data['goal'] = obj.goal
+        data['number'] = obj.number
+        data['project_id'] = obj.project_id
+        status = 200
+    except:
+        data['message'] = 'Not Found'
+        status = 404
+    return JsonResponse(data, status=status)
